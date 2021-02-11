600a673b98cb1a113ffa2b36e366c02f4f8e80ba
HarshaVenkat17 <49586960+HarshaVenkat17@users.noreply.github.com>
2021-02-11 21:35:56 +0530

    Update Booking.php

--- Booking.php
+++ Booking.php
@@ -1,8 +1,23 @@
 <html>
+<head>
+	<title>Confirmation</title> 
+</head>
 <body>
-
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+<?php
+	if($_GET["from"]=="--Select one--" or $_GET["to"]=="--Select one--" or $_GET["class"]=="--Select Class--" or $_GET["pref"]=="--Select Preference--" or $_GET["from"]==$_GET["to"])
+	{
+		header("Location: Main.php");
+		/*{<meta http-equiv = "refresh" content ='0 url = https://www.qries.com' />;}*/
+	}
+?>
+<?php echo $_GET["date"]; ?><br>
+<?php echo $_GET["from"]; ?><br>
+<?php echo $_GET["to"]; ?><br>
+<?php echo $_GET["ad"]; ?><br>
+<?php echo $_GET["ch"]; ?><br>
+<?php echo $_GET["phone"]; ?><br>
+<?php echo $_GET["class"]; ?><br>
+<?php echo $_GET["pref"]; ?><br>
 
 </body>
 </html>
