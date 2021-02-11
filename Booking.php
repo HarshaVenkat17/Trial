69ada0947a6240f91cef8a6e2a2eb8edd1fc8a0c
HarshaVenkat17 <49586960+HarshaVenkat17@users.noreply.github.com>
2021-02-11 22:54:13 +0530

    Update Booking.php

--- Booking.php
+++ Booking.php
@@ -1,15 +1,27 @@
 <html>
 <head>
 	<title>Confirmation</title> 
+	
 </head>
 <body>
 <?php
 	if($_GET["from"]=="--Select one--" or $_GET["to"]=="--Select one--" or $_GET["class"]=="--Select Class--" or $_GET["pref"]=="--Select Preference--" or $_GET["from"]==$_GET["to"])
 	{
 		header("Location: Main.php");
-		/*{<meta http-equiv = "refresh" content ='0 url = https://www.qries.com' />;}*/
 	}
-?>
+	$x=$_GET["from"];
+	$y=$_GET["to"];
+	$lenf = strlen($x);
+	$lent = strlen($y);
+	$sf=0;
+	$st=0;
+	for($i = 0; $i < $lenf; ++$i) 
+	    $sf=$sf+ord($from[$i]);
+	for($i = 0; $i < $lent; ++$i) 
+	    $st=$st+ord($to[$i]);
+	$cost= abs($st-$sf)*rand(1,5);
+	?>  
+
 <?php echo $_GET["date"]; ?><br>
 <?php echo $_GET["from"]; ?><br>
 <?php echo $_GET["to"]; ?><br>
